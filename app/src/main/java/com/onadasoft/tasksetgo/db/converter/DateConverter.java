9875b9dda503a6ddc6d3410409f09d57d1f3b7b9
BerserkerLurker <ahmed.karim.lahmar@gmail.com>
2018-06-30 23:18:15 +0100

    feat: add DateConverter

--- app/src/main/java/com/onadasoft/tasksetgo/db/converter/DateConverter.java
+++ app/src/main/java/com/onadasoft/tasksetgo/db/converter/DateConverter.java
@@ -1,4 +1,28 @@
+/*
+ * Created by Ahmed Karim on 30/06/18 20:46
+ * Copyright (c) 2018 . All rights reserved.
+ * Last modified 30/06/18 20:46
+ */
+
 package com.onadasoft.tasksetgo.db.converter;
 
+import android.arch.persistence.room.TypeConverter;
+
+import org.threeten.bp.OffsetDateTime;
+import org.threeten.bp.format.DateTimeFormatter;
+
 public class DateConverter {
+
+    private static DateTimeFormatter formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
+
+    @TypeConverter
+    public static OffsetDateTime toOffsetDateTime(String value){
+        return value == null ? null : formatter.parse(value, OffsetDateTime.FROM);
+    }
+
+    @TypeConverter
+    public String fromOffsetDateTime(OffsetDateTime date){
+        return date == null ? null : date.format(formatter);
+    }
+
 }
