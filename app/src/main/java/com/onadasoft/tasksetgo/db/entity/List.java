2b3c10cb8708dc000a8d6c3125df5feef9350d70
BerserkerLurker <ahmed.karim.lahmar@gmail.com>
2018-06-30 23:16:17 +0100

    feat: add List entity

--- app/src/main/java/com/onadasoft/tasksetgo/db/entity/List.java
+++ app/src/main/java/com/onadasoft/tasksetgo/db/entity/List.java
@@ -1,4 +1,48 @@
+/*
+ * Created by Ahmed Karim on 30/06/18 15:56
+ * Copyright (c) 2018 . All rights reserved.
+ * Last modified 30/06/18 15:56
+ */
+
 package com.onadasoft.tasksetgo.db.entity;
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "list_table")
 public class List {
+
+    @PrimaryKey(autoGenerate = true)
+    @NonNull
+    @ColumnInfo(name = "id")
+    private int mId;
+
+    @NonNull
+    @ColumnInfo(name = "name")
+    private String mName;
+
+    @ColumnInfo(name = "color")
+    private int mColor;
+
+    public List(int mId, @NonNull String mName, int mColor) {
+        this.mId = mId;
+        this.mName = mName;
+        this.mColor = mColor;
+    }
+
+    @NonNull
+    public int getId() {
+        return mId;
+    }
+
+    @NonNull
+    public String getName() {
+        return mName;
+    }
+
+    public int getColor() {
+        return mColor;
+    }
 }
