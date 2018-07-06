02c2feb0fdcf7e7ab94a3e8263e9641a17ed3294
BerserkerLurker <ahmed.karim.lahmar@gmail.com>
2018-07-06 21:11:18 +0100

    feat: instantiate AppExecutors
    get AppDatabase singleton
    
    get DataRepository singleton

--- app/src/main/java/com/onadasoft/tasksetgo/TaskSetGo.java
+++ app/src/main/java/com/onadasoft/tasksetgo/TaskSetGo.java
@@ -9,12 +9,26 @@ package com.onadasoft.tasksetgo;
 import android.app.Application;
 
 import com.jakewharton.threetenabp.AndroidThreeTen;
+import com.onadasoft.tasksetgo.concurrence.AppExecutors;
+import com.onadasoft.tasksetgo.db.AppDatabase;
 
 public class TaskSetGo extends Application {
+
+    private AppExecutors mAppExecutors;
     @Override
     public void onCreate() {
         super.onCreate();
 
         AndroidThreeTen.init(this);
+
+        mAppExecutors = new AppExecutors();
+    }
+
+    public AppDatabase getDatabase(){
+        return AppDatabase.getInstance(this, mAppExecutors);
+    }
+
+    public DataRepository getRepository(){
+        return DataRepository.getInstance(getDatabase());
     }
 }
